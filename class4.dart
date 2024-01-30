a7e2021e9086713064a4ed31fa4eac1b0cf8740a
jeftounz <srjesusc@gmail.com>
2024-01-30 13:08:35 -0400

    Conversion de tipo de variables

---
+++ class4.dart
@@ -0,0 +1,16 @@
+import 'dart:ffi';
+
+void main(){
+
+  /*CONVERSION DE TIPO DE DATOS*/
+  
+  int edad=20.2.toInt(); //De flotante oh double a entero
+  edad=int.parse("25"); //De string a entero
+  double precio=double.parse("55.1");
+
+  int codigoPostal=123456;
+
+  String name=300.toString(); //De numero a string
+  bool esCliente=false;
+  
+}
