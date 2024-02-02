7813071b2a97efc252e056e842b2433811768ae1
jeftounz <srjesusc@gmail.com>
2024-02-01 23:26:28 -0400

    Operadores de comparacion

---
+++ module2/class8.dart
@@ -0,0 +1,14 @@
+//OPERADORES DE COMPARACION
+
+void main(){
+  int edadMax=30;
+  int edadMin=18;
+  int edad=15;
+  String traeRegalo="no";
+  print(edad>edadMin);
+  print(edad<edadMax);
+  print(edad>=edadMin);
+  print(edad<=edadMax);
+  print(edad==edadMax);
+  print(traeRegalo!="no");
+}
