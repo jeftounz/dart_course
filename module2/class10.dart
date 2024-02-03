04f55ae359d30b5d007cae037c37d6d81a72c928
jeftounz <srjesusc@gmail.com>
2024-02-03 00:23:20 -0400

    OPERADORES DE ASIGNACION

---
+++ module2/class10.dart
@@ -0,0 +1,18 @@
+//OPERADORES DE ASIGNACION
+
+void main(){
+  int a=30;
+  int b=18;
+  
+  a+=b;
+  print(a);
+  a-=b;
+  print(a);
+  a*=b;
+  print(a);
+  a~/=b;
+  print(a);
+  a%=b;
+  print(a);
+
+}
