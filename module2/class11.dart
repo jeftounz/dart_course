e674bf9bf043a504554512c80fb503f00c5a268f
jeftounz <srjesusc@gmail.com>
2024-02-04 21:58:18 -0400

    Prueba operadores logicos

---
+++ module2/class11.dart
@@ -0,0 +1,10 @@
+//OPERADORES LOGICOS
+void main(){
+  int compra=1000;
+  int precioMax=2000;
+  bool esFrecuente=true;
+  bool tieneDeuda=false;
+
+  print(compra>=precioMax || esFrecuente && !tieneDeuda);
+  
+}
