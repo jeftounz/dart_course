78835cbce6e17207e25e981e2476b0e9c7fac046
jeftounz <srjesusc@gmail.com>
2024-02-05 23:19:15 -0400

    Sentencias if . else if y else

---
+++ module3/class13.dart
@@ -0,0 +1,32 @@
+void main()
+{
+  int calification=7;
+  int calificationMin=6;
+  if(calification>=calificationMin)
+  {
+    print("PASASTE!");
+  }else
+  {
+    print("No pasaste");
+  }
+
+  if(calification==10)
+  {
+    print("Exelente");
+  }else if(calification==9)
+  {
+    print("Muy bien");
+  }else if(calification==8)
+  {
+    print("Bien");
+  }else if(calification==7)
+  {
+    print("Regular");
+  }else if(calification==calificationMin)
+  {
+    print("Suficiente");
+  }else
+  {
+     print("No pasaste");
+  }
+}
