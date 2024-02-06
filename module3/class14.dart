f96a9bdf4365a2e918c8c26cb52eb2d406a0152f
jeftounz <srjesusc@gmail.com>
2024-02-05 23:29:15 -0400

    Sentencia Switch

---
+++ module3/class14.dart
@@ -0,0 +1,30 @@
+void main()
+{
+  int calification=7;
+  
+  switch (calification) {
+    case 10:
+       print("Exelente");
+      break;
+    case 9:
+       print("Muy bien");
+      break;
+
+    case 8:
+      print("Bien");
+      break;
+
+    case 7:
+      print("Regular");
+      break;
+
+    case 6:
+      print("Suficiente");
+      break;  
+
+    default:
+      print("No pasaste");
+      break;
+  }
+ 
+}
