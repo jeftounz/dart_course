2322d23a76e09e8310f41bdb573b1ea8214bc991
jeftounz <srjesusc@gmail.com>
2024-02-05 22:59:37 -0400

    Operadores condicionales

---
+++ module2/class12.dart
@@ -0,0 +1,13 @@
+//OPERADORES CONDICIONALES
+import 'dart:async';
+
+void main()
+{
+  int edad=20;
+  int edadMin=18;
+  print((edad>=edadMin)?"Puedes entrar":"No puedes entrar");
+
+  int x=15;
+  print(x??10);
+
+}
