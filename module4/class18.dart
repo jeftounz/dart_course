75351702c064d1fac7c604865136efb72e671a98
jeftounz <srjesusc@gmail.com>
2024-02-09 20:31:48 -0400

    Listas en dart

---
+++ module4/class18.dart
@@ -0,0 +1,14 @@
+//Listas 
+void main()
+{
+  //Lista heterogenea
+  List colores=["Azul","Verde","Rosa","Morado",1];
+  print(colores[3]);
+
+  //Lista homogenea
+  List<String> colores2=["Jesus","daniel","Gonzalez","Diego","Evies"];
+  colores2.add("Gabriel");
+  print(colores2); 
+
+
+}
