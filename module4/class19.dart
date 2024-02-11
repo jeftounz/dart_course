88f6ce8fc439b364e0a5acdb199f1a141124946c
jeftounz <srjesusc@gmail.com>
2024-02-10 23:44:22 -0400

    Recorrido de una estructura MAP

--- module4/class19.dart
+++ module4/class19.dart
@@ -12,4 +12,24 @@ void main()
   datos["Apellido"]="Castellanos";
   datos["email"]="correo@example.com";
   print(datos);
+
+  //RECORRIDO DE UN MAP
+  //Primera forma , su llave y su valor
+  for(String key in datos.keys)
+  {
+    print("Key : $key Valor:${datos[key]}");
+  }
+  //Segunda forma, se imprime los valores
+  for(String value in datos.keys)
+  {
+    print("Valor:$value");
+  }
+  //Tercera forma, se obtiene llave y valor utilizando algo llamado mapEntry
+  for(MapEntry mapEntry in datos.entries)
+  {
+    print("Valor: ${mapEntry.value}");
+  }
+  //Cuarta forma, utilizando un forEach
+  datos.forEach((key, value)=>print("key $key valor $value"));
+
 }
