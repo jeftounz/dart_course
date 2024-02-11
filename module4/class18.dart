2240aa5f1f09fde342c734a9bc313a9d3b02f882
jeftounz <srjesusc@gmail.com>
2024-02-10 22:51:45 -0400

    METODOS UTILES DE UNA LISTA

--- module4/class18.dart
+++ module4/class18.dart
@@ -23,4 +23,24 @@ void main()
   }
   
   colores2.forEach((String color)=>print(color));
+
+
+  //Metodos utiles de una lista
+  //.sublist(inidice1,indice2) creamos una sublista a partir de una primera posicion hasta una ultima posicion
+  List<String> colors=["Azul","Verde","Rojo","Amarillo","Morado"];
+  print(colors.sublist(2,4));
+  //.shuffle acomoda la lista de forma aleatoria
+  colors.shuffle();
+  print(colors);
+  //Tenemos el metodo .reversed que coloca la lista a la inversa
+  print(colors.reversed);
+  //Tenemos el metodo .where que nos permite buscar un elemento en base a una condicion
+  print(colors.where((String color) => color=="Morado"));
+
+  List<int> nums=[1,2,3,4,5];
+  //El metodo .reduce (solo funciona con listas con numeros), hace una sumatoria total de todos los elementos de la lista
+  print(nums.reduce((value, element)=>value+element));
+  //Tenemos el metodo .fold que es similar , pero con la pequeña diferencia que puedes agregar un valor inicial para sumar 
+   var resultado=nums.fold(10,(value, element)=>value+element);
+  print(resultado);
 }
