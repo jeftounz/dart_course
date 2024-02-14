f5f51726f16fdab602200956f8339cd494beb8f7
jeftounz <srjesusc@gmail.com>
2024-02-13 22:17:25 -0400

    TIPOS DE PARAMETROS

--- module5/class20.dart
+++ module5/class20.dart
@@ -4,7 +4,7 @@ void main()
 {
   
   bienvenida("Cristian de Jesus","Nuevo");
-  
+  TiposDatos("Dato1",2);
   
 }
 //Las funciones tienen parametros
@@ -28,4 +28,11 @@ bienvenida(String nombre, String tipo)
 descuento(String tipo,int descuento)
 {
   print("Por ser un usuario $tipo te ofrecemos un descuento de $descuento %");
+}
+//Los parametros [] son opcionales posicionales
+//Los parametros {} son opcionales con nombres
+TiposDatos(String tipo,[int])
+{
+  print("$tipo+$int");
+  
 }
