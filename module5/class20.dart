c022f0c70b3172b44cc08480e8dae62515ed3139
jeftounz <srjesusc@gmail.com>
2024-02-13 23:03:26 -0400

    Retornar valores

--- module5/class20.dart
+++ module5/class20.dart
@@ -2,10 +2,11 @@
 
 void main()
 {
-  
+  String recibirData;
   bienvenida("Cristian de Jesus","Nuevo");
   TiposDatos("Dato1",2);
-  
+  recibirData=Devolver();
+  print("Cristian de jesus $recibirData");
 }
 //Las funciones tienen parametros
 bienvenida(String nombre, String tipo)
@@ -35,4 +36,12 @@ TiposDatos(String tipo,[int])
 {
   print("$tipo+$int");
   
+}
+
+//Devolver datos
+Devolver()
+{
+  String despedida="nos vemos luego!";
+
+  return despedida; 
 }
